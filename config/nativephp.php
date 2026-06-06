f806c392792b38f725ad6a4acd99ba22ebb7ddc0
Hussain Khalid <hussain@smart-daddy.com>
2026-06-06 09:34:56 +0500

    Upgrade dependencies: bump Laravel framework, smart-till/core, boost, mcp, and sail in `composer.lock`.

--- config/nativephp.php
+++ config/nativephp.php
@@ -8,7 +8,7 @@ return [
      * It is used to determine if the app needs to be updated.
      * Increment this value every time you release a new version of your app.
      */
-    'version' => '1.0.22',
+    'version' => '1.0.23',
 
     /**
      * The ID of your application. This should be a unique identifier
